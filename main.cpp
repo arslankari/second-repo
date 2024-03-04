289e8e9d66b0cd392b314a5830754439a5df4b2c
arslankari <arslankarim899@gmail.com>
2024-03-04 19:55:01 +0500

    firstcommit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int savings;
+    cout<<"enter your savings";
+    cin>>savings;
+    if(savings>5000){
+        cout<<"party with friends";
+    }
+    else{
+        cout<<"stay at home";
+    }
+    return 0;
+}
